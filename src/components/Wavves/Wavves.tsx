c0651565f1541ca5bdd169ccc441830e43ce3668
adamatronix <you@example.com>
2022-05-03 16:52:05 +0800

    single-char Split up the string as separate span elements with spaces added

--- src/components/Wavves/Wavves.tsx
+++ src/components/Wavves/Wavves.tsx
@@ -5,6 +5,12 @@ const Wrapper = styled.div`
   position: relative;
 `
 
+const CharWrapper = styled.span`
+  position: relative;
+  display: inline-block;
+
+`
+
 interface WavvesProps {
   /**
    * Is this the principal call to action on the page?
@@ -14,9 +20,17 @@ interface WavvesProps {
 
 const Wavves = ({children, ...props}: WavvesProps) => {
 
+  const splitChars = (text: string) => {
+    const chars = text ? text.split('') : null;
+    return chars && chars.length > 0 ? chars.map((char) => {
+      const isSpace = char === ' ';
+      return !isSpace ? (<CharWrapper>{char}</CharWrapper>) : (<> </>);
+    }) : false;
+  }
+
   return (
     <Wrapper {...props}>
-      { children }
+      { splitChars(children) }
     </Wrapper>
   )
 }
