1692484a4de29bd7715251a30c2cc31b5a841da0
adamatronix <you@example.com>
2022-05-03 18:55:42 +0800

    single-char Add stagger animation

--- src/components/Wavves/Wavves.tsx
+++ src/components/Wavves/Wavves.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import gsap from 'gsap';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
   position: relative;
+  overflow: hidden;
 `
 
 const CharWrapper = styled.span`
   position: relative;
   display: inline-block;
+  transform:  translate(0,100%);
 
 `
 
@@ -19,12 +22,43 @@ interface WavvesProps {
 }
 
 const Wavves = ({children, ...props}: WavvesProps) => {
+  const charCache = useRef<(HTMLDivElement | null)[]>([]);
+  const characterTotal = children ? children.split('').filter((val)=> val !== ' ').length : 0;
+
+
+  const setupStagger = () => {
+    gsap.to(charCache.current, {
+      y: '0%',
+      stagger: {
+        each: 0.04
+      }
+    })
+  }
+
+  const useChar = (index:number) => {
+    const setRefs = useCallback(
+      (node:HTMLDivElement) => {
+        if(node !== null) {
+          charCache.current[index] = node;
+
+          if(charCache.current.length === characterTotal) {
+            setupStagger();
+          }
+        }
+      },
+      [charCache.current],
+    );
+    return [setRefs];
+  }
 
   const splitChars = (text: string) => {
     const chars = text ? text.split('') : null;
-    return chars && chars.length > 0 ? chars.map((char) => {
+    let countIndex = 0;
+    return chars && chars.length > 0 ? chars.map((char,i) => {
       const isSpace = char === ' ';
-      return !isSpace ? (<CharWrapper>{char}</CharWrapper>) : (<> </>);
+      const [ reference ] = useChar(countIndex);
+      countIndex = !isSpace ? countIndex + 1 : countIndex;
+      return !isSpace ? (<CharWrapper ref={reference}>{char}</CharWrapper>) : (<> </>);
     }) : false;
   }
 
